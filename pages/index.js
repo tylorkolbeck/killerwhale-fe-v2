cc91903ba4c4e79737354996388f36f00b51fc24
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2021-10-28 00:05:48 -0400

    add marquee

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import HomepageHero from '../sections/HomepageHero/HomepageHero.component'
+import TradeScroller from '../sections/TradeScroller/TradeScroller.component'
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <HomepageHero bgImage='/images/background_1.png' />
+      <TradeScroller />
     </div>
   )
 }
