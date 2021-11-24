af36477384986032682632c2183001fd63729b0a
Tylor <tylor.kolbeck@gmail.com>
2021-11-24 16:37:51 -0500

    refactor

--- pages/api/twitter-feed.js
+++ pages/api/twitter-feed.js
@@ -1,35 +1,52 @@
-export default function handler(req, res) {
-  const needle = require('needle')
-  const token = process.env.TWITTER_BEARER_TOKEN
-  const endpointUrl = 'https://api.twitter.com/2/tweets/search/recent'
+const axios = require('axios')
 
-  async function getRequest() {
-    const params = {
-      query: 'from:DylanShively6',
-      max_results: '20'
-    }
+let lastTweets = []
 
-    const res = await needle('get', endpointUrl, params, {
-      headers: {
+// App rate limit: 450 requests per 15-minute window shared among all users of your app
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    const token = process.env.TWITTER_BEARER_TOKEN
+    const endpointUrl = process.env.TWITTER_TWEETS_API_URL
+    const twitterAccount = `from:${process.env.TWITTER_ACCOUNT}`
+    const MAX_RESULTS = 20
+
+    async function fetchTweets() {
+      const params = {
+        query: twitterAccount,
+        max_results: MAX_RESULTS
+      }
+
+      const headers = {
         'User-Agent': 'v2RecentSearchJS',
         authorization: `Bearer ${token}`
       }
-    })
 
-    if (res.body) {
-      return res.body
+      return axios.get(endpointUrl, {
+        headers,
+        params
+      })
+    }
+
+    let tweets
+    let returnError
+
+    await fetchTweets()
+      .then(({ data }) => {
+        if (data.errors) {
+          lastTweets = data.data
+          tweets = lastTweets
+        } else {
+          tweets = data.data
+        }
+      })
+      .catch((err) => {
+        returnError = err.message
+      })
+
+    if (!returnError) {
+      res.status(200).json({ tweets })
     } else {
-      return
+      res.status(500).json({ error: returnError })
     }
   }
-
-  ;(async () => {
-    try {
-      const tweets = await getRequest()
-      res.status(200).json({ tweets })
-      // console.log(response)
-    } catch (e) {
-      console.log(e)
-    }
-  })()
 }
--- sections/TradeScroller/TradeScroller.component.js
+++ sections/TradeScroller/TradeScroller.component.js
@@ -1,49 +1,46 @@
 import styles from './TradeScroller.module.scss'
 import Marquee from 'react-fast-marquee'
 import useSWR from 'swr'
+import React from 'react'
 
 export default function TradeScroller() {
   const tweetFetcher = (url) => fetch(url).then((r) => r.json())
 
   const { data, error } = useSWR('/api/twitter-feed', tweetFetcher)
 
-  console.log(data)
+  function formatTweet(text, id) {
+    let stringArray = text.split(' ')
+    let profitIndex = stringArray.findIndex((string) => {
+      return string.includes('%')
+    })
+
+    let profitStringIndex = profitIndex + 1
+
+    return (
+      <React.Fragment key={id}>
+        <p>
+          {stringArray.map((string, index) => {
+            if (index === profitIndex || index === profitStringIndex) {
+              return <span className='text-green'>{string + ' '}</span>
+            } else {
+              return string + ' '
+            }
+          })}
+        </p>
+        <span style={{ margin: '0 1.5rem' }}>|</span>
+      </React.Fragment>
+    )
+  }
 
   if (error) return
-  if (!data)
+  if (!data || data.tweets.length === 0)
     return <div className={styles.loading}>Loading twitter feed. . .</div>
+
   return (
     <div className={styles.tradeScroller}>
       <Marquee gradient={false}>
-        <p>{data.tweets.data[0].text}</p>
-        <p>{data.tweets.data[1].text}</p>
-        <p>{data.tweets.data[2].text}</p>
-        <p>{data.tweets.data[3].text}</p>
-        <p>{data.tweets.data[4].text}</p>
-        <p>{data.tweets.data[5].text}</p>
-        <p>{data.tweets.data[6].text}</p>
-        <p>{data.tweets.data[7].text}</p>
-        <p>{data.tweets.data[8].text}</p>
-        <p>{data.tweets.data[9].text}</p>
-        <p>{data.tweets.data[10].text}</p>
-        <p>{data.tweets.data[11].text}</p>
-        <p>{data.tweets.data[12].text}</p>
-        <p>{data.tweets.data[13].text}</p>
-        <p>{data.tweets.data[14].text}</p>
-        <p>{data.tweets.data[15].text}</p>
-        <p>{data.tweets.data[16].text}</p>
-        <p>{data.tweets.data[17].text}</p>
-        <p>{data.tweets.data[18].text}</p>
-        <p>{data.tweets.data[19].text}</p>
-        {/* <p>
-          Signal just sold #XRP with{' '}
-          <span className='text-green fs-200'>1.88% profit</span> on #kucoin!
-          <span className='text-accent fs-100' style={{ marginLeft: 4 }}>
-            3hr
-          </span>
-        </p> */}
+        {data.tweets.map(({ text, id }) => formatTweet(text, id))}
       </Marquee>
     </div>
   )
 }
-// App rate limit: 450 requests per 15-minute window shared among all users of your app
