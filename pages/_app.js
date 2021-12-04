08fc4619dff68411dfc0bb0a1dd9abf7c4bb68da
jkaplanis <johntkaplanis@gmail.com>
2021-12-04 00:00:14 -0800

    connect article page to cms, add starter for articles layout

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,15 @@ import '../styles/typography.css'
 import '../styles/reset.css'
 
 import GlobalLayout from '../layouts/global/global.layout.component'
+import DefaultLayout from '../components/layouts/DefaultLayout'
 
 function MyApp({ Component, pageProps }) {
+  const Layout = Component.Layout || DefaultLayout
   return (
     <GlobalLayout>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </GlobalLayout>
   )
 }
