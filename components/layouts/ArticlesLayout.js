5160f46743c4c02b9e237bc105a1b49a87634030
Cole Whitman <colewhitman91@gmail.com>
2021-12-05 19:45:49 -0500

    Initial layout for articles page

--- components/layouts/ArticlesLayout.js
+++ components/layouts/ArticlesLayout.js
@@ -1,5 +1,29 @@
 import React from 'react'
+import styles from './ArticlesLayout.module.scss'
 
 export default function ArticlesLayout({ children }) {
-  return <div style={{ color: 'yellow' }}>{children}</div>
+  return (
+    <>
+      <div className='container'>
+        <div className={styles.topPromo}>Promo 1</div>
+        <div className={styles.grid}>
+          <div>{children}</div>
+          <div className={styles.sideBar}>
+            sidebar: Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Sint quibusdam cumque vel, autem aliquid nulla iste tempora
+            repudiandae nobis, corporis excepturi animi impedit. Veniam quidem
+            incidunt inventore non! Dolor, dignissimos? Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Cupiditate, culpa! Excepturi
+            voluptas quidem blanditiis expedita, ea officia nesciunt! Excepturi
+            repellendus at neque cum labore cumque numquam, sapiente cupiditate
+            magnam soluta. Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Aperiam iure perspiciatis dolorum perferendis doloribus
+            nesciunt consequuntur, error qui. Totam sint earum aut facilis
+            aspernatur provident voluptas. Fuga libero fugit quae.
+          </div>
+        </div>
+        <div className={styles.bottomPromo}>Promo 2</div>
+      </div>
+    </>
+  )
 }
--- pages/article/[slug].js
+++ pages/article/[slug].js
@@ -1,10 +1,24 @@
+// import Image from 'next/image' // Do all of the images have the same domain?
+import styles from './article.module.scss'
 import ArticlesLayout from '../../components/layouts/ArticlesLayout'
 import { getAllPostsIds, getPostData } from '../../lib/posts'
 import { fetchAPI } from '../../lib/api'
 
 const Article = ({ postData }) => {
-  console.log(postData)
-  return <h1>hello world</h1>
+  // console.log(postData)
+  return (
+    <>
+      <div>
+        <h1 className={styles.title}>{postData.title}</h1>
+        <img src={postData.image.url} alt={postData.image.alternativeText} />
+        <h2 className={styles.description}>{postData.description}</h2>
+        <p className={styles.author}>
+          {postData.author.name}, {postData.created_at}
+        </p>
+        <p className={styles.content}>{postData.content}</p>
+      </div>
+    </>
+  )
 }
 
 Article.Layout = ArticlesLayout
