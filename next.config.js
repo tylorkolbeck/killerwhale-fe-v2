9a7d7df164b55b986b855c9c54927f3412bd0efb
jkaplanis <johntkaplanis@gmail.com>
2021-12-07 21:56:11 -0800

    add markdown component, bodytext component, bulletItem component, add material ui and react markdown libraries

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  reactStrictMode: true,
+  images: {
+    domains: [
+      'localhost',
+      process.env.NEXT_PUBLIC_STRAPI_API_URL,
+      'duj27fxoqhxbb.cloudfront.net'
+    ]
+  },
+  reactStrictMode: true
 }
--- pages/article/[slug].js
+++ pages/article/[slug].js
@@ -1,10 +1,19 @@
 import ArticlesLayout from '../../components/layouts/ArticlesLayout'
 import { getAllPostsIds, getPostData } from '../../lib/posts'
 import { fetchAPI } from '../../lib/api'
+import Image from 'next/image'
+import { getStrapiMedia } from '../../utils/media'
 
 const Article = ({ postData }) => {
-  console.log(postData)
-  return <h1>hello world</h1>
+  console.log(postData.image.url)
+  return (
+    <Image
+      src={getStrapiMedia(postData.image.url)}
+      alt='image'
+      height='200'
+      width='400'
+    ></Image>
+  )
 }
 
 Article.Layout = ArticlesLayout
