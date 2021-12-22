fa782cad0101eb3558e4e94e2bab20e898ec4563
Tylor <tylor.kolbeck@gmail.com>
2021-12-22 15:11:58 -0500

    add zendesk chat widget

--- components/Chat/Chat.component.js
+++ components/Chat/Chat.component.js
@@ -2,7 +2,7 @@ import Script from 'next/script'
 
 export default function Chat() {
   const zendeskKey = process.env.NEXT_PUBLIC_ZENDESK_KEY
-  const chatSource = `https:static.zdassets.com/ekr/snippet.js?key=${zendeskKey}`
+  const chatSource = `https://static.zdassets.com/ekr/snippet.js?key=${zendeskKey}`
 
   return <Script id='ze-snippet' src={chatSource} />
 }
