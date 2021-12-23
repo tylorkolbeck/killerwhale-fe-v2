40aaca1be1c53fa83d82803ae28e52a55b01f669
jkaplanis <johntkaplanis@gmail.com>
2021-12-22 21:51:08 -0800

    add google tag manager tag to app

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,12 @@ import '../styles/reset.css'
 
 import GlobalLayout from '../layouts/global/global.layout.component'
 import DefaultLayout from '../components/layouts/DefaultLayout'
+import TagManager from 'react-gtm-module'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-TKHK54R' })
+  }, [])
   const Layout = Component.Layout || DefaultLayout
   return (
     <GlobalLayout>
