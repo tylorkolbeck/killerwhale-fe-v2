e57bda1c0a718d490a633bc40bcbad2b797712aa
jkaplanis <johntkaplanis@gmail.com>
2022-01-31 22:45:19 -0800

    add cookie consent component and cookie-policy page

---
+++ components/CookieConsent/CookieConsent.component.js
@@ -0,0 +1,44 @@
+import CookieConsent from 'react-cookie-consent'
+import Link from 'next/link'
+
+function CookieConsentPopUp() {
+  return (
+    <CookieConsent
+      location='bottom'
+      buttonText=' OK '
+      style={{
+        background: 'rgba(105,105,105, 0.75)',
+        borderRadius: '25px',
+        margin: '15px',
+        zIndex: '100',
+        padding: '25px',
+        maxWidth: '400px',
+        fontSize: '13px'
+      }}
+      buttonStyle={{
+        background: '#24ae90',
+        borderRadius: '15px',
+        fontSize: '13px',
+        color: 'white'
+      }}
+    >
+      <h2>Cookie Consent</h2>
+      We use cookies to provide and improve our services. By continuing to use
+      our site you consent to cookies. Read more on our{' '}
+      <Link href='/privacy-policy'>
+        <a style={{ textDecoration: 'underline', color: 'white' }}>
+          privacy policy
+        </a>
+      </Link>{' '}
+      and{' '}
+      <Link href='/cookie-policy'>
+        <a style={{ textDecoration: 'underline', color: 'white' }}>
+          cookie policy
+        </a>
+      </Link>
+      .{' '}
+    </CookieConsent>
+  )
+}
+
+export default CookieConsentPopUp
